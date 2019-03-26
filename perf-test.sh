84464e59beca8f39f9f0784cbf8b4d7582ad28ab
Donnacha Oisín Kidney <mail@doisinkidney.com>
2019-03-26 11:39:19 +0000

    proper perf test

--- perf-test.sh
+++ perf-test.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 find . -name '*.agda' | xargs -n1 -L1 agda
+
+recompile () {
+    echo $1
+    rm "$1.agdai"
+    time agda "$1.agda"
+}
+
+recompile "Data/Binary/NonZero/Tests/Addition"
+recompile "Data/Binary/Segmented/Tests/Addition"
+recompile "Data/Binary/Distrib/Tests/Addition"
+recompile "Data/Binary/NonZero/Tests/Multiplication"
+recompile "Data/Binary/Segmented/Tests/Multiplication"
+recompile "Data/Binary/Distrib/Tests/Multiplication"
